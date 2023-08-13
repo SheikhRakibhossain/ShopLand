3dc4f3acb2713f9f7f297c48eed9ac295d236d97
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-08-13 18:08:21 +0600

    user observer created

--- src/components/AuthProvider/AuthProvider.jsx
+++ src/components/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -16,6 +16,20 @@ const AuthProvider = ({children}) => {
     const logIn = (email, password)=>{
         return signInWithEmailAndPassword(auth, email, password)
     }
+
+    //observer creation with return observation function
+    useEffect( ()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+            console.log("Current user login", currentUser);
+            setUser(currentUser);
+        
+        })
+        return () =>{
+            unsubscribe()
+        }
+    },
+    []
+    )
     const authInfo = {
         user,
         createUser,
